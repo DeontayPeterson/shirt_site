f56b94bbe9a7ede8e1a25bc6d9c4e3d57daf5ab9
DeontayPeterson <rnmurphy47@gmail.com>
2023-04-28 01:04:25 -0400

    add views for shirt items

--- project/admin/views.py
+++ project/admin/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from project.user import User
 from .forms import LoginForm, AddForm, RemoveForm
 from project.models import Shirt
+from project import db 
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates/admin')
 
@@ -22,25 +23,50 @@ def login():
             flash('Invalid Username or Password', 'danger')
     return render_template('login.html', form=form)
 
+
+
 @admin_blueprint.route('/logout',)
 @login_required
 def logout():
+
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('index'))
 
+
 # Route for admin dashboard
-@admin_blueprint.route('/dashboard',) #methods=['POST', 'GET'])
+@admin_blueprint.route('/dashboard',methods=['GET']) 
 @login_required
 def dashboard():  
 
     addform = AddForm()
     removeform = RemoveForm()
-
     if not current_user.is_admin:
         flash("You don't have permission to view this page.", 'danger')
         return redirect(url_for('main.idex'))
+    
     return render_template('dashboard.html', forms={'addform': addform, 'removeform': removeform})
 
+
+@admin_blueprint.route('/dashboard/add_shirt', methods=['POST'])
+@login_required
+def add_shirt():
+
+    addform = AddForm()
+    #TODO Add error handling. 
+    if request.method == "POST":
+        shirt = Shirt(name=addform.name.data, price=addform.price.data, description=addform.description.data,
+                        thumbnail=addform.thumbnail.data, )
+        db.session.add(shirt)
+        db.session.commit()
+        flash('Shirt has been added to the database')
+    
+    return redirect(url_for('admin.dashboard'))
+
+
+@admin_blueprint.route('/dashboard/view') 
+@login_required
+def view():
+    return render_template('view.html')
 
 
